1d1ccf4a435b000a3551228b12bb7f2ef55ed99c
Ubuntu <ubuntu@ip-172-31-18-18.ec2.internal>
2020-11-28 06:40:41 +0000

    Add review list screen

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,4 @@
 class Book < ApplicationRecord
-    has_one_attached :image
+    has_one_attached :image  # 1 book co mot anh -- dinh nghia quan he cho bang book voi image
+    has_many :book_review, -> { order "created_at DESC" }
 end
